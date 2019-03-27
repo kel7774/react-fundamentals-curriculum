ab617d4205741b4ef475260c4bd2a933bde05c21
Kelli Landry <kelli.landry@waitrapp.com>
2019-03-27 16:17:26 -0500

    combine reducers
    refactor todos to switch statement from if statement
    remove json
    make app turn goals and todo into objects

--- app/index.js
+++ app/index.js
@@ -1,47 +1,33 @@
-{
-    type: "TO_DO",
-    todo: {
-        id: 0,
-        name: "Learn Redux",
-        complete: false,
-    }
-}
-
-{
-    type: 'REMOVE_TODO',
-    id: 0,
-}
-
-{
-   type: "TOGGLE_TODO",
-   id: 0,
-}
-
-{
-    type: "ADD_GOAL",
-    goal: {
-        id: 0,
-        name: "Run a marathon"
-    }
-}
-
-{
-    type: "REMOVE_GOAL",
-    id: 0
-}
-
 //Reducer function
 function todos(state = [], action) {
-    if (action.type === "ADD_TODO"){
-        return state.concat([action.todo])
-    } else if (action.type === "REMOVE_TODO"){
-        return state.filter((todo) => todo.id !== action.id)
-    } else if (action.type === "TOGGLE_TODO"){
-        return state.map((todo) => todo.id !== action.id ? todo : 
-        Object.assign({}, todo, {complete: !todo.complete})
+    switch (action.type) {
+        case 'ADD_TODO':
+            return state.concat([action.todo])
+        case "REMOVE_TODO":
+            return state.filter((todo) => todo.id !== action.id)
+        case "TOGGLE_TODO":
+            return state.map((todo) => todo.id !== action.id ? todo : 
+            Object.assign({}, todo, {complete: !todo.complete})
         )
-    } else {
+    default:
         return state
+    }
+}
+
+function goals(state = [], action){
+    switch(action.type){
+        case "ADD_GOAL":
+            return state.concat([action.goal])
+        case "REMOVE_GOAL":
+            return state.filter((goal) => goal.id !== action.id)
+        default: state
+    }
+}
+
+function app(state = {}, action){
+    return {
+        todos: todos(state.todos, action),
+        goals: goals(state.goals, action)
     }
 }
 
