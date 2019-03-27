56ee9feb1e1fda316b1aae43dba008bff85421ae
Kelli Landry <kelli.landry@waitrapp.com>
2019-03-27 14:24:05 -0500

    Add actions to update state

--- app/index.js
+++ app/index.js
@@ -1,3 +1,37 @@
+{
+    type: "TO_DO",
+    todo: {
+        id: 0,
+        name: "Learn Redux",
+        complete: false,
+    }
+}
+
+{
+    type: 'REMOVE_TODO',
+    id: 0,
+}
+
+{
+   type: "TOGGLE_TODO",
+   id: 0,
+}
+
+{
+    type: "ADD_GOAL",
+    goal: {
+        id: 0,
+        name: "Run a marathon"
+    }
+}
+
+{
+    type: "REMOVE_GOAL",
+    id: 0
+}
+
+//action is just an object that represents an event that will change the state of our store
+
 function createStore(){
     //the store should have 4 parts
     //1. the state
